da2b06378d55a648b5a64e67d0af560d604bf241
youssefzaoin <youssefzaoin24@gmail.com>
2015-01-25 18:21:19 +0100

    supercalcolatore

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(int argc, char** argv) {
     float a, b;
     int scelta;
     float risultato, frazionale;
+    int esponente;
     
     puts("Benvenuto nel SuperCaolcolatore, il piu' innovativo software di calcolo presente sul mercato.\n");
     printf("Inserisci a, il primo operando (frazionale): ");
@@ -13,7 +14,7 @@ int main(int argc, char** argv) {
     scanf("%f", &b);
     
     getchar();
-    /* La presenza di questa chiamata alla funzione getchar() Ã¨ dovuto al funzionamento
+    /* La presenza di questa chiamata alla funzione getchar() è dovuto al funzionamento
      * "buffered line" definito dallo standard POSIX.
      */
     
@@ -23,34 +24,41 @@ int main(int argc, char** argv) {
     puts("3 -  Moltiplicazione a*b");
     puts("4 -  Divisione a/b con risultato frazionale");
     puts("5 -  Disione a/b con parte intera e parte frazionale");
-    puts("6 -  Elevamento a potenza a^b (l'esponente Ã¨ troncato ad intero)");
+    puts("6 -  Elevamento a potenza a^b (l'esponente è troncato ad intero)");
     scelta = getchar();
     scelta = scelta - '0';
     
     switch(scelta) {
         case 1:
-            risultato = 0;
+            risultato = a+b;
             printf("%f+%f=%f\n", a, b, risultato);
             break;
         case 2:
-            risultato = 0;
+            risultato = a-b;
             printf("%f-%f=%f\n", a, b, risultato);
             break;
         case 3:
-            risultato = 0;
+            risultato = a*b;
             printf("%f*%f=%f\n", a, b, risultato);
             break;
         case 4:
-            risultato = 0;
+            risultato = a/b;
             printf("%f/%f=%f\n", a, b, risultato);
             break;
         case 5:
-            risultato = 0;
-            frazionale = 0;
+            risultato = (int)a/b;
+            frazionale = a/b - risultato;
             printf("%f/%f= %d (intera) e %f (frazionale)\n", a, b, (int)risultato, frazionale);
             break;
         case 6:
-            risultato = 0;
+            risultato = 1;
+            esponente = (int)b;
+            if(esponente < 0)
+                esponente = -esponente;
+            while(esponente > 0) {
+                risultato *= a;
+                esponente--;
+            }
             printf("%f^%d=%f\n", a, (int)b, risultato);
             break;
         default:
